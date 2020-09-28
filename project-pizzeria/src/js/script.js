0c90204444367753a0d82ad906da8d902ddceddc
Krzysztof Badelek <k.badelek@gmail.com>
2020-09-28 22:20:17 +0200

    add function processOrder

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -57,8 +57,11 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
-      console.log('new product: ' , thisProduct);
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
+      
     }
     renderInMenu(){
       const thisProduct = this;
@@ -73,12 +76,26 @@
       //add element to menu
       menuContainer.appendChild(thisProduct.element);
     }
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      //console.log('accordion: ', thisProduct.accordionTrigger);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      //console.log('form: ', thisProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      //console.log('formInputs: ', thisProduct.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      //console.log('cartButton: ', thisProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      //console.log('priceElem: ', thisProduct.priceElem);
+    }
     initAccordion(){
       const thisProduct = this;
   
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableElement = thisProduct.element.querySelector(select.menuProduct.clickable);
-      console.log('clickableElement: ',  clickableElement);
+      const clickableElement = thisProduct.accordionTrigger;
+      
       /* START: click event listener to trigger */
       clickableElement.addEventListener('click' , function(event){
         /* prevent default action for event */
@@ -102,13 +119,68 @@
       /* END: click event listener to trigger */
       });
     }
+    initOrderForm(){
+      const thisProduct = this;
+      console.log('initOrderForm: ');
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+    processOrder(){
+      const thisProduct = this;
+    
+      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      /* set variable price to equal thisProduct.data.price */
+      let price = thisProduct.data.price;
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for( let paramId in thisProduct.data.params){
+        /* save the element in thisProduct.data.params with key paramId as const param */
+        const param = thisProduct.data.params[paramId];
+        console.log('param: ', param);
+        /* START LOOP: for each optionId in param.options */
+        for(let optionId in param.options){
+          /* save the element in param.options with key optionId as const option */
+          const option = param.options[optionId];
+          console.log('option: ', option);
+          /* START IF: if option is selected and option is not default */
+          const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+          if(optionSelected && !option.default){
+            /* add price of option to variable price */
+            price = price + option.price;
+          /* END IF: if option is selected and option is not default */
+          }
+          /* START ELSE IF: if option is not selected and option is default */
+          else if(!optionSelected && option.default){
+            /* deduct price of option from price */
+            price = price - option.price;
+          
+          /* END ELSE IF: if option is not selected and option is default */
+          }
+        /* END LOOP: for each optionId in param.options */
+         }
+      /* END LOOP: for each paramId in thisProduct.data.params */
+      }
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+      thisProduct.priceElem = price;
+    }
   }
-  
-
   const app = {
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.data: ' , thisApp.data);
+      
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }
